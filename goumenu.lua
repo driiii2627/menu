149030497efe517931db25d46f09b1b580b405c6
rayzmd <kaicolivsantos@gmail.com>
2025-09-30 22:01:52 -0300

    Update goumenu.lua with proper menu structure

---
+++ goumenu.lua
@@ -0,0 +1,88 @@
+-- Menu Principal
+-- Script Lua para interface de menu
+
+local menu = {}
+
+-- Configurações do menu
+menu.config = {
+    width = 800,
+    height = 600,
+    title = "Menu Principal"
+}
+
+-- Função para inicializar o menu
+function menu.init()
+    print("Inicializando menu...")
+    -- Carregar assets
+    menu.loadAssets()
+    -- Configurar interface
+    menu.setupUI()
+end
+
+-- Função para carregar assets
+function menu.loadAssets()
+    -- Carregar imagens de background
+    menu.background = "assets/base do menu, background.png"
+    menu.logo = "assets/logo menu/logo menu.png"
+    menu.playerIcon = "assets/icons menu/icon da boneco jogador.png"
+    
+    -- Carregar elementos de interface
+    menu.tabSelected = "assets/tab/indicação de tab selecionada.png"
+    menu.tabNormal = "assets/tab/tab do menu quando selecionado.png"
+end
+
+-- Função para configurar interface
+function menu.setupUI()
+    -- Configurar tabs
+    menu.setupTabs()
+    -- Configurar área de funções
+    menu.setupFunctions()
+end
+
+-- Função para configurar tabs
+function menu.setupTabs()
+    menu.tabs = {
+        {name = "Principal", active = true},
+        {name = "Configurações", active = false},
+        {name = "Sobre", active = false}
+    }
+end
+
+-- Função para configurar área de funções
+function menu.setupFunctions()
+    menu.functions = {
+        "Função 1",
+        "Função 2", 
+        "Função 3"
+    }
+end
+
+-- Função principal de renderização
+function menu.render()
+    -- Renderizar background
+    menu.renderBackground()
+    -- Renderizar tabs
+    menu.renderTabs()
+    -- Renderizar área de funções
+    menu.renderFunctions()
+end
+
+-- Função para renderizar background
+function menu.renderBackground()
+    -- Código para renderizar o background
+end
+
+-- Função para renderizar tabs
+function menu.renderTabs()
+    -- Código para renderizar as tabs
+end
+
+-- Função para renderizar área de funções
+function menu.renderFunctions()
+    -- Código para renderizar a área de funções
+end
+
+-- Inicializar o menu
+menu.init()
+
+return menu
